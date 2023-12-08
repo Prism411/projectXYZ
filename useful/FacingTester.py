852263473ee70c85790ef7c7ace7c87490b92e15
Prism411 <jaderlouis@proton.me>
2023-12-08 10:20:37 -0300

    getCord and performance improvements

--- useful/FacingTester.py
+++ useful/FacingTester.py
@@ -11,46 +11,117 @@ import numpy as np
 # 1366x768 (-6 nos y)
 #xi1, yi1, xf2, yf2 = 138, 243, 154, 259
 
-def identificarCord(imagem, tolerancia):
-    # Define a região de interesse
-    regiao_interesse = (0, 0, 16, 16)
+# regiao_interesse = (160, 0, 176, 16)  (6 quadradinhos) pior caso possivel.
+# regiao_interesse = (142, 0, 158, 16)  (5 quadradinhos) caso menos ruim
+# regiao_interesse = (124, 0, 140, 16)  (4 quadradinhos) caso nao tao ruim assim.
+# regiao_interesse = (106, 0, 122, 16)  (3 quadradinhos) melhor caso possivel.
+#18 pixels de diferença por aqui, tomar cuidado com o vlaor a ser plotado
+#fazer alguma função que distribua esses valores.
+
+## coordenada do quadrado é (xi=0, yi=0, xf=16, yf=16)
+# a cada quadradinho lido o proximo vai estar na coordenadas (xi=0+18, yi=0, xf=16+18, yf=16) entao ela le ele e ve se encontra
+# a imagem para comparar, o nome dela (nome_temp) pode ser entre "0 e 9", se nao encotrar nenhum dos numeros ela tenta encontrar um (nome_temp) "ponto"
+#caso seja um ponto ela ira ler apenas mais um quadradinho e ira printar os numeros que leu a partir das comparaçoes
+# e assim sucessivamente
+#      regiao_interesse = (xi, yi, xf, yf=)
+#     regiao_cortada = imagem.crop(regiao_interesse)
+#     salvar_path = "cortetemp.png"
+#     regiao_cortada.save(salvar_path)
+#     referencia_path = "../database/ponto.png"
+#     if comparar_imagens(salvar_path, referencia_path, tolerancia):
+#         print("ponto ENCONTRADO!")
+#           ((aqui ele só ira ler mais um quadrado))
+#           e terminara
+#         os.remove(salvar_path)
+#     else:
+#         print("cancela")
+def detectar_sinal(imagem, regiao_interesse, referencia_path, nome_temp, tolerancia):
     regiao_cortada = imagem.crop(regiao_interesse)
+    salvar_path = f"quadradotemp{nome_temp}.png"
+    regiao_cortada.save(salvar_path)
+
+    if comparar_imagens(salvar_path, referencia_path, tolerancia):
+        os.remove(salvar_path)
+        return str(nome_temp)
+    else:
+        os.remove(salvar_path)
+        return ""
+
+def getCord(imagem, tolerancia):
+    #quadrado1 = (18, 0, 34, 16) faça uma função que começando desta coordenada
+    #ela vai procurar aqui referencia_paths = [f"../database/{i}.png" for i in range(10)]
+    #na função: detectar_sinal(imagem, regiao_interesse, referencia_path, nome_temp, tolerancia):
+    #regiao_cortada = imagem.crop(regiao_interesse)
+    #salvar_path = f"quadradotemp{nome_temp}.png"
+    #regiao_cortada.save(salvar_path)
+
+    #if comparar_imagens(salvar_path, referencia_path, tolerancia):
+    #    os.remove(salvar_path)
+    #    return str(nome_temp)
+   # else:
+    #    os.remove(salvar_path)
+    #    return ""
+    #ela vai fazer a verificação
+
+
+    #regiao_cortada = imagem.crop(quadrado1)
+    #salvar_path = "quadradotemp.png"
+    #regiao_cortada.save(salvar_path)
+
+    referencia_paths = [f"../database/{i}.png" for i in range(10)]
+
+    #for referencia_path in referencia_paths:
+       # if detectar_sinal(imagem, quadrado1, referencia_path,"temp", tolerancia):
+           # return str(referencia_paths.index(referencia_path))
+
+    # Se não encontrou nenhum número, tenta encontrar um ponto
+    ponto_referencia_path = "../database/ponto.png"
+    if detectar_sinal(imagem, quadrado1, ponto_referencia_path,"temp", tolerancia):
+        # Leitura de mais um número após encontrar o ponto
+        xi, xf = 18, 34
+        cord_detectada = ""
+        while xi < imagem.width:
+            regiao_interesse = (xi, 0, xf, 16)
+            for referencia_path in referencia_paths:
+                if detectar_sinal(imagem, regiao_interesse, referencia_path,"temp", tolerancia):
+                    cord_detectada += str(referencia_paths.index(referencia_path))
+                    xi += 18
+                    xf += 18
+                    break
+            else:
+                break  # Sai do loop se não encontrar um número
+
+        return cord_detectada
+
+    return "Nenhum número ou ponto detectado"
+
+
+def identificarCord(imagem, tolerancia):
+    quadrado1 = (0, 0, 16, 16)
+    regiao_cortada = imagem.crop(quadrado1)
     salvar_path = "quadradotemp.png"
     regiao_cortada.save(salvar_path)
-    # Caminho completo para a imagem de referência
-    referencia_path = "../database/(.png"
-    # Compara a região de interesse com a imagem de referência
-    if comparar_imagens(salvar_path, referencia_path, tolerancia):
-        print("beleza")
+
+    referencia_path1 = "../database/(.png"
+    if comparar_imagens(salvar_path, referencia_path1, tolerancia):
+        print("( foi detectado")
         os.remove(salvar_path)
     else:
-        print("cancela")
+        print("Cancela")
 
-    regiao_interesse = (18, 0, 34, 16)
-    regiao_cortada = imagem.crop(regiao_interesse)
-    salvar_path = "quadradotemp2.png"
-    regiao_cortada.save(salvar_path)
-    referencia_path = "../database/traco.png"
-    if comparar_imagens(salvar_path, referencia_path, tolerancia):
-        print("beleza")
-        os.remove(salvar_path)
-    else:
-        print("cancela")
+    # Defina as regiões de interesse e caminhos de referência para os sinais
+    regioes_sinais = [
+        ((18, 0, 34, 16), "../database/traco.png", "teste"),
+        ((160, 0, 176, 16), "../database/traco.png", "teste"),
+        ((142, 0, 158, 16), "../database/traco.png", "teste"),
+        ((124, 0, 140, 16), "../database/traco.png", "teste"),
+        ((106, 0, 122, 16), "../database/traco.png", "teste"),
+    ]
 
+    for i, (regiao_interesse, referencia_path, nome_temp) in enumerate(regioes_sinais, start=1):
+        print(detectar_sinal(imagem, regiao_interesse, referencia_path, nome_temp, tolerancia))
 
-    regiao_interesse = (106, 0+6, 121, 16+6)
-    regiao_cortada = imagem.crop(regiao_interesse)
-    salvar_path = "quadradotemp3.png"
-    regiao_cortada.save(salvar_path)
-    referencia_path = "../database/traco.png"
-    if comparar_imagens(salvar_path, referencia_path, tolerancia):
-        print("beleza")
-        os.remove(salvar_path)
-    else:
-        print("cancela")
-
-    regiao_interesse = (18, 0, 34, 16)
-
+    print("detectado = "+ getCord(imagem,tolerancia))
 
 # faça aqui uma função que recebe uma letra e retorna os numeros para realizar crop
 #se a letra for e ou w ele fará o corte xi = 138 + 428 xf = 254 + 621
